c5206226ad658d741c28afb0a83675942517a959
rdorado <ruben.dorados@gmail.com>
2015-11-14 21:51:59 -0500

    Added counter and stats pages

--- itemcounter.php
+++ itemcounter.php
@@ -1,29 +1,45 @@
 <?php
 
-class Config
-{
-    public static function read($filename)
-    {
-        $config = include $filename;
-        return $config;
-    }
-    public static function write($filename, array $config)
-    {
-        $config = var_export($config, true);
-        file_put_contents($filename, "<?php return $config ;");
-    }
-}
-
 
 class ItemCounter
 {
+   private static $link = null ;
+
+   public static function count($url)
+   {
+      if (!$config = parse_ini_file('itemcounter.ini', TRUE)) throw new exception('Unable to open itemcounter.ini.');
+
+      return $config['counter']['url']."?u=".$url;
+   }
 
 }
 
-MyConfig::write('conf1.txt', array( 'setting_1' => 'foo' ));
-$config = MyConfig::read('conf1.txt');
-$config['setting_1'] = 'bar';
-$config['setting_2'] = 'baz';
-MyConfig::write('conf1.txt', $config);
+
+
+/**
+Before:
+**/
+
+$words = array("Life" => 3, "is" => 7, "beautiful" => 19);
+foreach($words as $word => $key){
+
+  $url = "dictionary.php?w=$key";
+  print "<a href='$url'>$word</a><br>";
+}
+
+
+/**
+After:
+**/
+
+$words = array("Life" => 3, "is" => 7, "beautiful" => 19);
+foreach($words as $word => $key){
+
+  $url = ItemCounter::count("dictionary.php?w=$key");
+  print "<a href='$url'>$word</a><br>";
+}
+
+
+//echo "<a href=".ItemCounter::count('image.jpg').">IMG</a>";
 
 ?>
