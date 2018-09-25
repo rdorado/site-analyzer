40e16daa504add3e68ba0108d93ec736bc193cb1
Ruben Dorado <ruben.dorados@gmail.com>
2018-09-25 17:50:59 -0400

    Create SiteAnalyzer.php

---
+++ src/SiteAnalyzer.php
@@ -0,0 +1,24 @@
+<?php
+
+/**
+ *
+ * (c) Ruben Dorado <ruben.dorados@google.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace SiteAnalyzer;
+
+
+/**
+ * class SiteAnalyzer
+ *
+ * @package   SiteAnalyzer
+ * @author    Ruben Dorado <ruben.dorados@gmail.com>
+ * @copyright 2018 Ruben Dorado
+ * @license   http://www.opensource.org/licenses/MIT The MIT License
+ */
+class SiteAnalyzer{
+
+}
