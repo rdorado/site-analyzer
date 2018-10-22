a6e8e69f5624b9bf7d0fcc9a4c1c6d4886f008bd
Ruben Dorado <ruben.dorados@gmail.com>
2018-10-21 20:14:44 -0400

    Fixing the package

---
+++ src/Sit|eAnalyzerException.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ *
+ * (c) Ruben Dorado <ruben.dorados@google.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace SiteAnalyzer;
+
+use Exception;
+
+
+/**
+ * class SiteAnalyzer
+ *
+ * @package   DatabaseException
+ * @author    Ruben Dorado <ruben.dorados@gmail.com>
+ * @copyright 2018 Ruben Dorado
+ * @license   http://www.opensource.org/licenses/MIT The MIT License
+ */
+class SiteAnalyzerException extends Exception{
+
+}
+
--- src/example.php
+++ src/example.php
@@ -1,4 +1,6 @@
 <?php 
+require_once __DIR__ . '/vendor/autoload.php';
+
 
 //include_once 'SiteAnalyzer.php';
 use SiteAnalyzer\SiteAnalyzer;
